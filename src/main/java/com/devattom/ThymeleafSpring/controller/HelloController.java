653b0275c0f85ae9b098a3647cb18d8244f82d00
Rousselin Thomas <98778784+Devattom@users.noreply.github.com>
2025-06-04 13:08:53 +0200

    add form (#3)
    Co-authored-by: Thomas ROUSSELIN <t.rousselin@mtarget.fr>

--- src/main/java/com/devattom/ThymeleafSpring/controller/HelloController.java
+++ src/main/java/com/devattom/ThymeleafSpring/controller/HelloController.java
@@ -3,17 +3,21 @@ package com.devattom.ThymeleafSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
 
 @Controller
 public class HelloController {
+    @GetMapping("/showForm")
+    public String showForm() {
+        return "hello-form";
+    }
 
-    @GetMapping("/hello")
-    public String sayHello(Model model) {
+    @RequestMapping("/processForm")
+    public String processForm(Model model) {
         model.addAttribute("message", "Hello World!");
         model.addAttribute("date", LocalDateTime.now());
-
         return "hello";
     }
 }
