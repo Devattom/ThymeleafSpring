6ac8bde5b7d5559ff5e03c9d2701e9fd143d28a5
Rousselin Thomas <98778784+Devattom@users.noreply.github.com>
2025-06-05 13:22:05 +0200

    try form binding with text fields (#6)
    Co-authored-by: Thomas ROUSSELIN <t.rousselin@mtarget.fr>

---
+++ src/main/java/com/devattom/ThymeleafSpring/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.devattom.ThymeleafSpring.controller;
+
+import com.devattom.ThymeleafSpring.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showStudentForm(Model model) {
+        model.addAttribute("student", new Student());
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processStudentForm(@ModelAttribute("student") Student student) {
+
+        System.out.println("student: " + student.getFirstName() + " " + student.getLastName());
+        return "student-confirmation";
+    }
+}
